6a97a59a09f26c5f38e23f2c59b313a89b35e493
Anshu Sagar <yuvraaj.anshu@gmail.com>
2021-01-02 00:08:25 +0530

    js file

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 document.getElementById("btn").addEventListener("click",referpage)
 function referpage(){
-    window.location.href="community-sub.html"
+    window.location.href="discussion.html"
 }
+function updateData(){
+    var com_div=document.getElementById("community-contents");
+    var div=document.createElement("div");
+    div.setAttribute("class","flex-row-content");
+    
 
-
-
-function recieveData(){
-    console.log("clicked")
+    com_div.append(div);
 }
 
--- sub.js
+++ sub.js
@@ -1,17 +1,19 @@
 
-
 document.getElementById("btn-1").addEventListener("click",(event)=>{
     event.preventDefault();
     var topic = document.getElementById("topic").selectedIndex;
-    var subtopic=document.getElementById("subtopic").selectedIndex;
     var title=document.getElementById("title").value;
     var message=document.getElementById("Message").value;
-    // console.log(message);
     localStorage.setItem("topic",document.getElementsByTagName("option")[topic].value);
     localStorage.setItem("title",title);
     localStorage.setItem("message",message);
-    var h1=document.getElementById("subtopic");
-    console.log(h1)
+    referpage()
+    
     
 
 });
+
+function referpage(){
+    
+    window.location.href="Community.html"
+}
