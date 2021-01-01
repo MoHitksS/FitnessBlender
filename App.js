9a487d0af3c69bb6431a97ce8105debdd6bebb61
Anshu Sagar <yuvraaj.anshu@gmail.com>
2021-01-02 00:13:22 +0530

    js file

--- App.js
+++ App.js
@@ -5,9 +5,25 @@ function referpage(){
 function updateData(){
     var com_div=document.getElementById("community-contents");
     var div=document.createElement("div");
-    div.setAttribute("class","flex-row-content");
     
+    var data=`<div class="flex-row-content">
+    <div>
+        <a class="anc" href="">${localStorage.getItem("title")}</a>
+        <P>By Fitness Blender</P>
+    </div>
+    <div>
+        <p>FB Team / Announcements</p>
+    </div>
+    <div>
+        <p>1</p>
+    </div>
+    <div>
+        <p>57</p>
+    </div>
+</div>`;
 
+        div.innerHTML=data;
     com_div.append(div);
 }
+updateData();
 
