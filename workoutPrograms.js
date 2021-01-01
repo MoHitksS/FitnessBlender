8655933264b655f8ff159a726fdb92bf0e59b8fa
mandarsatam <33372941+mandarsatam@users.noreply.github.com>
2021-01-01 16:45:42 +0530

    api to workout program

--- workoutPrograms.js
+++ workoutPrograms.js
@@ -1,54 +1,62 @@
-let items = document.querySelectorAll(".add-to-bag");
+const getData = async () => {
+	try {
+		const response = await fetch("http://localhost:3000/programs", {
+			method: "GET",
+		});
+		const data = await response.json();
+		console.log(data);
+		let output = `
+		<div class="exclusive__card yellow">
+			<div class="exclusive__card__content">
+				<h1><span class="fb-transperant">FB</span>Store</h1>
+				<h2>EGIFT CARDS</h2>
+				<hr />
+				<p>New eGift cards availabile!</p>
+			</div>
+		</div>
 
-let products = [
-	{
-		name:
-			"FB Low Impact Round 2 - Build Muscle & Burn Fat - 40 Minutes or Less",
-		price: 11.99,
-		img:
-			"https://d18zdz9g6n5za7.cloudfront.net/plan/1020/1020-o_31_fb-low-impact-round-2-fat-loss-program-40-minutes-or-less.jpg",
-
-		tag: "11",
-	},
-	{
-		name: "FB Blend - Burn Fat, Build Muscle, Tone; 35 or 55 Minutes a Day",
-		price: 12.99,
-		img:
-			"https://d18zdz9g6n5za7.cloudfront.net/plan/320/320-o_29_4-week-fb-blend-burn-fat-build-muscle-tone-35-or-55-minutes-a-day.jpg",
-
-		tag: "22",
-	},
-	{
-		name: "FB 30 - Fat Loss Program For Busy People (Round 4)",
-		price: 13.99,
-		img:
-			"https://d18zdz9g6n5za7.cloudfront.net/plan/320/320-o_28_fb-30-4-week-fat-loss-program-for-busy-people-round-4.jpg",
-
-		tag: "33",
-	},
-	{
-		name: "FB Strong - 4 Week Build Muscle, Burn Fat and Feel Great",
-		price: 14.99,
-		img:
-			"https://d18zdz9g6n5za7.cloudfront.net/plan/320/320-o_26_4-week-fb-strong-build-muscle-burn-fat-and-feel-great.jpg",
-
-		tag: "44",
-	},
-	{
-		name: "Add FB Meal Plan to your order and get 20% Off",
-		price: 15.99,
-		img:
-			"https://d18zdz9g6n5za7.cloudfront.net/plan/320/320-o_22_fb-bodyweight-no-equipment-workouts-to-burn-fat-and-tone-up.jpg",
-
-		tag: "55",
-	},
-];
-
-for (let i = 0; i < items.length; i++) {
-	items[i].addEventListener("click", () => {
-		itemNumbers(products[i]);
-	});
-}
+		<div class="exclusive__card red">
+			<div class="exclusive__card__content">
+				<h1><span class="fb-transperant">FB</span>Store</h1>
+				<h2>CHALLENGES</h2>
+				<hr />
+				<p>Short flexible programs for FB Plus members!</p>
+			</div>
+		</div>`;
+		for (var i in data) {
+			const product = data[i];
+			output =
+				output +
+				`
+				<div class="card">
+				<div class="card__img">
+					<img
+						src="${data[i].img}"
+						alt=""
+					/>
+				</div>
+				<div class="card__content">
+					<p>CORE - 35 MIN</p>
+					<h4>${data[i].name}</h4>
+				</div>
+				<div class="card__bottom">
+					<div class="bottom__left">
+						<h4>$${data[i].price}</h4>
+					</div>
+					<div class="bottom__right">
+						<button class="add-to-bag" onClick="itemNumbers(${data[i].tag})"> 
+							<i class="fas fa-shopping-bag"></i>ADD TO BAG
+						</button>
+					</div>
+				</div>
+			</div>
+		    `;
+		}
+		document.querySelector(".main").innerHTML = output;
+	} catch (err) {
+		console.log("This is the catch block: " + err);
+	}
+};
 
 function onLoadCartItems() {
 	let productNumbers = localStorage.getItem("itemNumbers");
@@ -57,20 +65,29 @@ function onLoadCartItems() {
 	}
 }
 
-function itemNumbers(product) {
-	let productNumbers = localStorage.getItem("itemNumbers");
-	productNumbers = Number(productNumbers);
-	if (productNumbers) {
-		localStorage.setItem("itemNumbers", productNumbers + 1);
-		document.querySelector(".header-menu span").textContent =
-			productNumbers + 1;
-	} else {
-		localStorage.setItem("itemNumbers", 1);
-		document.querySelector(".header-menu span").textContent = 1;
-	}
+const itemNumbers = async (tag) => {
+	try {
+		const response = await fetch(`http://localhost:3000/programs?tag=${tag}`, {
+			method: "GET",
+		});
+		const product = await response.json();
+		console.log(product[0]);
+		let productNumbers = localStorage.getItem("itemNumbers");
+		productNumbers = Number(productNumbers);
+		if (productNumbers) {
+			localStorage.setItem("itemNumbers", productNumbers + 1);
+			document.querySelector(".header-menu span").textContent =
+				productNumbers + 1;
+		} else {
+			localStorage.setItem("itemNumbers", 1);
+			document.querySelector(".header-menu span").textContent = 1;
+		}
 
-	setItem(product);
-}
+		setItem(product[0]);
+	} catch (err) {
+		console.log("This is the catch block: " + err);
+	}
+};
 
 function setItem(product) {
 	let cartItems = localStorage.getItem("productsInCart");
@@ -91,4 +108,5 @@ function setItem(product) {
 	localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
+getData();
 onLoadCartItems();
