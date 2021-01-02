c380fadefa37f773e9e0da0685b945324d520809
Himanshu Dwivedi <himrd95@gmail.com>
2021-01-02 11:41:31 +0530

    before login and after login html

--- script/signIn.js
+++ script/signIn.js
@@ -1,24 +1,32 @@
+// function check(form) {
+    // const data = JSON.parse(localStorage.getItem('userData'))
+    
+//         if (form.userid.value == "" || form.pswrd.value == "") {
+//             alert("Please Enter all the Details")
+//         }else {
+//             if (form.pswrd.value.length > 6) {
+//                 // for(i in data) {
+//                     if(form.userid.value == "myuserid" && form.pswrd.value == "password"){
+//                         window.open('afterLoginLanding.html','_self')
+//                     }
+//                     else{
+//                         alert("Error Password or Username")
+//                     }
+//                 // }
+//             }else{
+//                 alert('Password should be more than 6 letters')
+//             }
+//         }   
+// }   
 
-function submitBtn () {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const checkbox = document.getElementById('checkbox')
-    const data = JSON.parse(localStorage.getItem('userData'))
-    console.log(password.length)
-    if (username === "" && password === "") {
-        alert("Please Enter all the field")
-    }else {
-        if (password.length > 6) {
-            for(i in data) {
-                if (data[i].user === username && data[i].pass === password) {
-                    const x = document.getElementById('signInForm').action
-                    document.getElementById('display').innerHTML = x
-                }
-            }
-        }else{
-            alert('Password should be more than 6 letters')
+function check(form)
+    {
+        if(form.userid.value == "myuserid" && form.pswrd.value == "password")
+        {
+            window.open('afterLoginLanding.html','_self')
+        }
+        else
+        {
+            alert("Error Password or Username")
         }
     }
-}
-
-
