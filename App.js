bf17bfd51294ae83237a8dfe192604f65815ac5a
Anshu Sagar <yuvraaj.anshu@gmail.com>
2021-01-04 13:20:51 +0530

    discuss

--- App.js
+++ App.js
@@ -6,24 +6,26 @@ function updateData(){
     var com_div=document.getElementById("community-contents");
     var div=document.createElement("div");
     
-    var data=`<div class="flex-row-content">
+    var data=`
+    <div class="content-flex-col">
+    <div class="flex-row-content">
     <div>
         <a class="anc" href="">${localStorage.getItem("title")}</a>
-        <P>By Fitness Blender</P>
+        <P>${"By Anshu"}</P>
     </div>
     <div>
-        <p>FB Team / Announcements</p>
+        <p>${localStorage.getItem("topic")}</p>
     </div>
     <div>
-        <p>1</p>
+        <p>${Math.floor(Math.random()*10)}</p>
     </div>
     <div>
-        <p>57</p>
+        <p>${Math.floor(Math.random()*50)}</p>
     </div>
+</div>
 </div>`;
 
         div.innerHTML=data;
     com_div.append(div);
 }
 updateData();
-
